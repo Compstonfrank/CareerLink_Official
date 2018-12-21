b446442c8942c36474eb0c4bf756c941a4b0c2ce
Colleen Risso <colleenrisso@gmail.com>
2018-12-20 19:26:15 -0800

    updated components

--- client/components/Form_Employer/Form_Employer.js
+++ client/components/Form_Employer/Form_Employer.js
@@ -1,1 +1,10 @@
+/* eslint-disable camelcase */
 import React, { Component } from 'react';
+
+const Form_Employers = (props) => {
+    return (
+        <h1>FORM EMPLOYERS COMPONENT</h1>
+    )
+}
+
+export default Form_Employers;
