af3c0de736cf61774614984f4094e7653f5fe8e1
Colleen Risso <colleenrisso@gmail.com>
2018-12-21 15:44:21 -0800

    seed created

---
+++ server/db/seeders/seed_employers.js
@@ -0,0 +1,106 @@
+/* eslint-disable no-unused-vars */
+
+//The seed files here can be edited and then copy-pasted for use in the migration file.
+
+const employerSeed = [
+    {
+        name: 'WhiteFox Defense Techonologies, Inc',
+        email: 'info@whitefoxdefense.com',
+        logooUrl: 'http://sbdc.calpoly.edu/wp-content/uploads/2014/12/WhiteFox-logo.png',
+        LinkedInUrl: 'https://www.linkedin.com/company/whitefox-defense-technologies-inc./',
+        youtubeUrl: 'https://www.youtube.com/watch?v=sIhQMyBsSVE',
+        description: 'WhiteFox Defense Techonologies, Inc...',
+        industry: 'defense',
+        size: 'small/start-up'
+    },
+    {
+        name: 'SoCreate',
+        email: 'info@socreate.com',
+        logooUrl: 'https://definedictionarymeaning.com/assets/uploads/system/default-avatar-ponsy-deer.png',
+        LinkedInUrl: 'https://www.linkedin.com/company/so-create/',
+        youtubeUrl: 'https://www.youtube.com/watch?v=sIhQMyBsSVE',
+        description: 'SoCreate...',
+        industry: 'ecommerce / screenwriting',
+        size: 'small/start-up'
+    },
+    {
+        name: 'MINDBODY, Inc.',
+        email: 'info@mindbody.com',
+        logooUrl: 'https://levinjcc.org/wp-content/uploads/2018/07/Mindbody.png',
+        LinkedInUrl: 'https://www.linkedin.com/company/mindbodypowered/',
+        youtubeUrl: 'https://www.youtube.com/watch?v=sIhQMyBsSVE',
+        description: 'MINDBODY, Inc. ...',
+        industry: 'business management software',
+        size: 'large / public'
+    },
+    {
+        name: 'ITECH Solutions',
+        email: 'info@itech-solutions.com',
+        logooUrl: 'https://s3-media1.fl.yelpcdn.com/bphoto/RAClTIk1zMZ0D6HU9GnFyQ/ls.jpg',
+        LinkedInUrl: 'https://www.linkedin.com/company/itech-solutions_2/',
+        youtubeUrl: 'https://www.youtube.com/watch?v=sIhQMyBsSVE',
+        description: 'ITECH Solutions ...',
+        industry: 'web development',
+        size: 'unknown'
+    },
+    {
+        name: 'IQMS',
+        email: 'info@iqms.com',
+        logooUrl: 'https://pbs.twimg.com/profile_images/664870231823613952/8Mr0fVPH_400x400.png',
+        LinkedInUrl: 'https://www.linkedin.com/company/iqms/',
+        youtubeUrl: 'https://www.youtube.com/watch?v=sIhQMyBsSVE',
+        description: 'IQMS ...',
+        industry: 'manufacturing ERP',
+        size: 'large'
+    },
+    {
+        name: 'Clever Ducks',
+        email: 'info@cleverducks.com',
+        logooUrl: 'https://media.glassdoor.com/sqll/1044748/clever-ducks-squarelogo-1441216763187.png',
+        LinkedInUrl: 'https://www.linkedin.com/company/clever-ducks/',
+        youtubeUrl: 'https://www.youtube.com/watch?v=sIhQMyBsSVE',
+        description: 'Clever Ducks ...',
+        industry: 'IT Support',
+        size: 'unknown'
+    },
+    {
+        name: 'Land Gorilla',
+        email: 'info@landgorilla.com',
+        logooUrl: 'https://s.yimg.com/ny/api/res/1.2/YsBGg1qUWwf.009whWC9ZQ--~A/YXBwaWQ9aGlnaGxhbmRlcjtzbT0xO3c9ODAw/http://globalfinance.zenfs.com/en_us/Finance/US_AFTP_PRNEWSWIRE_LIVE/Land_Gorilla_Announces_Integration_With-a33991c3619056dcc69645318f7ec4f4',
+        LinkedInUrl: 'https://www.linkedin.com/company/land-gorilla/',
+        youtubeUrl: 'https://www.youtube.com/watch?v=sIhQMyBsSVE',
+        description: 'Land Gorilla ...',
+        industry: 'Construction Loan Software Developer',
+        size: 'unknown'
+    },
+    {
+        name: 'Random Dev Company',
+        email: 'info@randomdev.com',
+        logooUrl: 'https://www.citizendeveloper.com/wp-content/uploads/2018/02/happy-developers-1.png',
+        LinkedInUrl: 'https://www.linkedin.com/company/penguin-random-house-publishing/',
+        youtubeUrl: 'https://www.youtube.com/watch?v=sIhQMyBsSVE',
+        description: 'Random Dev Company ...',
+        industry: 'Random company',
+        size: 'unknown'
+    },
+    {
+        name: 'Google',
+        email: 'info@google.com',
+        logooUrl: 'https://pbs.twimg.com/profile_images/1057899591708753921/PSpUS-Hp_400x400.jpg',
+        LinkedInUrl: 'https://www.linkedin.com/company/google/',
+        youtubeUrl: 'https://www.youtube.com/watch?v=sIhQMyBsSVE',
+        description: 'The internet ...',
+        industry: 'internet',
+        size: 'maximum'
+    },
+    {
+        name: 'Amazon',
+        email: 'info@amazon.com',
+        logooUrl: 'http://g-ec2.images-amazon.com/images/G/01/social/api-share/amazon_logo_500500._V323939215_.png',
+        LinkedInUrl: 'https://www.linkedin.com/company/google/',
+        youtubeUrl: 'https://www.youtube.com/watch?v=sIhQMyBsSVE',
+        description: 'everything except the internet...',
+        industry: 'everything except the internet',
+        size: 'maximum'
+    },
+]
