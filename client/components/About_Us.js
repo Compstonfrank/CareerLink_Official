0a725a5930aa5868c69cc6e8778d747836ea2b7c
Colleen Risso <colleenrisso@gmail.com>
2018-12-20 23:28:16 -0800

    created footer

---
+++ client/components/About_Us.js
@@ -0,0 +1,10 @@
+/* eslint-disable camelcase */
+import React, { Component } from 'react';
+
+const About_Us = (props) => {
+    return (
+        <h1>This component is all about us and the building of our app.</h1>
+    )
+}
+
+export default About_Us;
--- client/components/Main.js
+++ client/components/Main.js
@@ -6,6 +6,7 @@ import Welcome from './Welcome';
 import Login from './Login/Login';
 import Sign_Up from './Sign_Up/Sign_Up';
 import User_Info from './User_Info/User_Info';
+import About_Us from './About_Us';
 import All_Employers from './All_Employers/All_Employers';
 import All_Students from './All_Students/All_Students';
 import Single_Employer from './Single_Employer/Single_Employer';
@@ -22,13 +23,16 @@ const Main = () => {
         <Link style={{ textDecoration: 'none', color: '#c0c0c0' }} to="/info"> My Info </Link>
         <Link style={{ textDecoration: 'none', color: '#c0c0c0' }} to="/"> Home </Link>
         <Link style={{ textDecoration: 'none', color: '#c0c0c0' }} to="/login"> Login </Link>
+        <Link style={{ textDecoration: 'none', color: '#c0c0c0' }} to="/about"> About Us </Link>
       </nav>
+
       <div>
         <Switch>
           <Route exact path="/" component={Welcome} />
           <Route exact path="/login" component={Login} />
           <Route exact path="/sign_up" component={Sign_Up} />
           <Route exact path="/info" component={User_Info} />
+          <Route exact path="/about_us" component={About_Us} />
           <Route exact path="/students" component={All_Students} />
           <Route exact path="/students/:studentId" component={Single_Student} />
           <Route exact path="/students/new_student" component={Form_Student} />
@@ -37,6 +41,14 @@ const Main = () => {
           <Route exact path="/employers/new_employer" component={Form_Employer} />
         </Switch>
       </div>
+
+      <footer id="footer">
+        <Link style={{ textDecoration: 'none', color: '#c0c0c0' }} to="/about"> About Us </Link>
+        <Link style={{ textDecoration: 'none', color: '#c0c0c0' }} to="/students/1"> Colleen Risso </Link>
+        <Link style={{ textDecoration: 'none', color: '#c0c0c0' }} to="/students/2"> Frank Compston </Link>
+        <Link style={{ textDecoration: 'none', color: '#c0c0c0' }} to="/students/3"> Justin Cook </Link>
+        <Link style={{ textDecoration: 'none', color: '#c0c0c0' }} to="/students/4"> Robin Lee </Link>
+      </footer>
     </div>
   );
 };
