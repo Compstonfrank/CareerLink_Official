3a258d8a8b055648bd47af6ae09a98fd8e9c8e0f
Frank Compston <compstonfrank@gmail.com>
2018-12-21 21:02:55 -0800

    we now have a functioning all students, i adjusted the webpack config so components render properly, and i renamed with company model as employer

--- client/components/Main.js
+++ client/components/Main.js
@@ -8,7 +8,7 @@ import Sign_Up from './Sign_Up/Sign_Up';
 import User_Info from './User_Info/User_Info';
 import About_Us from './About_Us';
 import All_Employers from './All_Employers/All_Employers';
-import All_Students from './All_Students/All_Students';
+import All_Students_Container from './All_Students/All_Students_Container';
 import Single_Employer from './Single_Employer/Single_Employer';
 import Single_Student from './Single_Student/Single_Student';
 import Form_Employer from './Form_Employer/Form_Employer';
@@ -18,12 +18,36 @@ const Main = () => {
   return (
     <div>
       <nav id="navbar">
-        <Link style={{ textDecoration: 'none', color: '#c0c0c0' }} to="/employers"> Employers </Link>
-        <Link style={{ textDecoration: 'none', color: '#c0c0c0' }} to="/students"> Students </Link>
-        <Link style={{ textDecoration: 'none', color: '#c0c0c0' }} to="/info"> My Info </Link>
-        <Link style={{ textDecoration: 'none', color: '#c0c0c0' }} to="/"> Home </Link>
-        <Link style={{ textDecoration: 'none', color: '#c0c0c0' }} to="/login"> Login </Link>
-        <Link style={{ textDecoration: 'none', color: '#c0c0c0' }} to="/about"> About Us </Link>
+        <Link
+          style={{ textDecoration: 'none', color: '#c0c0c0' }}
+          to="/employers"
+        >
+          {' '}
+          Employers{' '}
+        </Link>
+        <Link
+          style={{ textDecoration: 'none', color: '#c0c0c0' }}
+          to="/students"
+        >
+          {' '}
+          Students{' '}
+        </Link>
+        <Link style={{ textDecoration: 'none', color: '#c0c0c0' }} to="/info">
+          {' '}
+          My Info{' '}
+        </Link>
+        <Link style={{ textDecoration: 'none', color: '#c0c0c0' }} to="/">
+          {' '}
+          Home{' '}
+        </Link>
+        <Link style={{ textDecoration: 'none', color: '#c0c0c0' }} to="/login">
+          {' '}
+          Login{' '}
+        </Link>
+        <Link style={{ textDecoration: 'none', color: '#c0c0c0' }} to="/about">
+          {' '}
+          About Us{' '}
+        </Link>
       </nav>
 
       <div>
@@ -33,21 +57,56 @@ const Main = () => {
           <Route exact path="/sign_up" component={Sign_Up} />
           <Route exact path="/info" component={User_Info} />
           <Route exact path="/about_us" component={About_Us} />
-          <Route exact path="/students" component={All_Students} />
+          <Route exact path="/students" component={All_Students_Container} />
           <Route exact path="/students/:studentId" component={Single_Student} />
           <Route exact path="/students/new_student" component={Form_Student} />
           <Route exact path="/employers" component={All_Employers} />
-          <Route exact path="/employers/:employerId" component={Single_Employer} />
-          <Route exact path="/employers/new_employer" component={Form_Employer} />
+          <Route
+            exact
+            path="/employers/:employerId"
+            component={Single_Employer}
+          />
+          <Route
+            exact
+            path="/employers/new_employer"
+            component={Form_Employer}
+          />
         </Switch>
       </div>
 
       <footer id="footer">
-        <Link style={{ textDecoration: 'none', color: '#c0c0c0' }} to="/about"> About Us </Link>
-        <Link style={{ textDecoration: 'none', color: '#c0c0c0' }} to="/students/1"> Colleen Risso </Link>
-        <Link style={{ textDecoration: 'none', color: '#c0c0c0' }} to="/students/2"> Frank Compston </Link>
-        <Link style={{ textDecoration: 'none', color: '#c0c0c0' }} to="/students/3"> Justin Cook </Link>
-        <Link style={{ textDecoration: 'none', color: '#c0c0c0' }} to="/students/4"> Robin Lee </Link>
+        <Link style={{ textDecoration: 'none', color: '#c0c0c0' }} to="/about">
+          {' '}
+          About Us{' '}
+        </Link>
+        <Link
+          style={{ textDecoration: 'none', color: '#c0c0c0' }}
+          to="/students/1"
+        >
+          {' '}
+          Colleen Risso{' '}
+        </Link>
+        <Link
+          style={{ textDecoration: 'none', color: '#c0c0c0' }}
+          to="/students/2"
+        >
+          {' '}
+          Frank Compston{' '}
+        </Link>
+        <Link
+          style={{ textDecoration: 'none', color: '#c0c0c0' }}
+          to="/students/3"
+        >
+          {' '}
+          Justin Cook{' '}
+        </Link>
+        <Link
+          style={{ textDecoration: 'none', color: '#c0c0c0' }}
+          to="/students/4"
+        >
+          {' '}
+          Robin Lee{' '}
+        </Link>
       </footer>
     </div>
   );
