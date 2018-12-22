eb57d6bc16305a4b5703f912f1453f0d0c49cb4f
Frank Compston <compstonfrank@gmail.com>
2018-12-22 02:26:13 -0800

    just put login in a form

--- client/components/Login/Login.js
+++ client/components/Login/Login.js
@@ -3,38 +3,46 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 
+const Login = props => {
+  return (
+    <div>
+      <h1> USER IS ATTEMPTING TO LOG IN </h1>
+      <br />
+      <form>
+        <label> Email: </label>
+        <input type="text" name="email" placeholder="Email" />
 
-const Login = (props) => {
-    return (
+        <label> Password: </label>
+        <input type="text" name="password" placeholder="Password" />
+
         <div>
-            <h1> USER IS ATTEMPTING TO LOG IN </h1>
-            <br />
-
-            <label> Email: </label>
-            <input type="text" name="email" placeholder="Email" />
-            <br />
-
-            <label> Password: </label>
-            <input type="text" name="password" placeholder="Password" />
-            <br />
-
-            <div>
-                <button type="submit"> Login </button>
-                {' '}
-                <button type="button" id="facebook"> Login with Facebook </button>
-                <button type="button" id="github"> Login with GitHub </button>
-                <button type="button" id="google"> Login with Google </button>
-            </div>
-            <br />
-
-            <div>
-                <Link to="/sign_up">
-                    <button type="button" id="newUser"> Create New Account </button>
-                </Link>
-            </div>
-
+          <button type="submit"> Login </button>{' '}
+          <button type="button" id="facebook">
+            {' '}
+            Login with Facebook{' '}
+          </button>
+          <button type="button" id="github">
+            {' '}
+            Login with GitHub{' '}
+          </button>
+          <button type="button" id="google">
+            {' '}
+            Login with Google{' '}
+          </button>
         </div>
-    )
-}
+      </form>
+      <br />
+
+      <div>
+        <Link to="/sign_up">
+          <button type="button" id="newUser">
+            {' '}
+            Create New Account{' '}
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
+};
 
 export default Login;
