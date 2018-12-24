26d5edc65133576dd5c882d59d2ee223fe8904a1
Colleen Risso <colleenrisso@gmail.com>
2018-12-23 23:53:55 -0800

    All_Employers connected to Single_Employers. Single_Employers not fully functional yet.

--- client/components/Single_Employer/Single_Employer.js
+++ client/components/Single_Employer/Single_Employer.js
@@ -1,11 +1,32 @@
 /* eslint-disable camelcase */
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
 
-const Single_Employer = (props) => {
-    return (
-        <h1>SINGLE EMPLOYER COMPONENT</h1>
-    )
+export default class Single_Employer extends Component {
+    
+    componentDidMount(){
+        this.props.fetchSingleEmployer();
+    }
+
+    componentDidUpdate(prevProps){
+        if (prevProps.match.params.employerId !== this.props.match.params.employerId) {
+            this.props.fetchSingleEmployer();
+          }
+    }
+
+    render() {
+        const { employer } = this.props;
+        return (
+            <div key={employer.id}>
+                <img src={employer.photoUrl} id="allStudentPics" />
+                <h3>name: {employer.name}</h3>
+                <h4>industry: {employer.industry}</h4>
+                <p>email: {employer.email}</p>
+                <p>linkedIn: {employer.LinkedInUrl}</p>
+                <p>GitHub: {employer.gitHubUrl}</p>
+                <p>YouTube: {employer.youtubeUrl}</p>
+                <p>Phone: {employer.phoneNumber}</p>
+                <p>Description: {employer.description}</p>
+            </div>
+        )
+    }
 }
-
-export default Single_Employer;
