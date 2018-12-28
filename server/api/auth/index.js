9f37990ab8af49e5859aa780dbdd2fd2719c8a07
Justin Cook <justin.cook.dev@gmail.com>
2018-12-27 17:42:51 -0800

    Fixed the local error.

--- server/api/auth/index.js
+++ server/api/auth/index.js
@@ -3,10 +3,11 @@ const router = require('express').Router();
 
 //These routes are mounted on /api/auth
 
+
 //Link to O-Auth
-router.use('/local', require('./local'));
-router.use('/facebook', require('./facebook'));
-router.use('/github', require('./github'));
-router.use('/google', require('./google'));
+// router.use('/local', require('./local'));
+// router.use('/facebook', require('./facebook'));
+// router.use('/github', require('./github'));
+// router.use('/google', require('./google'));
 
 module.exports = router;
