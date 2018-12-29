41d8c0ea2d11495c2cf23823f4761b888d540542
Colleen Risso <colleenrisso@gmail.com>
2018-12-29 11:04:20 -0800

    added ternary to avoid rendering empty info in employers

--- client/components/Single_Employer/Single_Employer.js
+++ client/components/Single_Employer/Single_Employer.js
@@ -1,3 +1,4 @@
+/* eslint-disable complexity */
 /* eslint-disable camelcase */
 
 import React, { Component } from 'react';
@@ -20,22 +21,58 @@ export default class Single_Employer extends Component {
 
         return (
             <div key={employer.id}>
-                <img src={employer.logoUrl} id="allStudentPics" />
-                <h3>name: {employer.name}</h3>
-
-                <div id="youtube">
+                <div id="heading">
+                    <h1>{employer.name}</h1>
+                    {employer.logoUrl
+                        ? <img src={employer.logoUrl} id="allStudentPics" />
+                        : null
+                    }
+                </div>
+                
+                <div id="video">
                 {employer.youtubeUrl && employer.youtubeUrl.length > 0
                     ? <iframe width="640" height="390" src={`https://www.youtube.com/embed/${getVideoName(employer.youtubeUrl)}`} frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen={true} />
                     : <h4>This company's video is not currently available</h4>
                 }
                 </div>
 
-                <h4>industry: {employer.industry}</h4>
-                <p>email: {employer.email}</p>
-                <p>linkedIn: {employer.LinkedInUrl}</p>
-                <p>GitHub: {employer.gitHubUrl}</p>
-                <p>Phone: {employer.phoneNumber}</p>
-                <p>Description: {employer.description}</p>
+                <div id="singleAbout">
+                    <h4>About {employer.name}</h4>
+                    {employer.description
+                        ? <p>{employer.description}</p>
+                        : <p>No description available yet</p>
+                    }
+                    {employer.industry
+                        ? <p>Industry: {employer.industry}</p>
+                        : null
+                    }
+                </div>
+
+                <div id="contactInfo">
+                    <h4>Contact Information</h4>
+                    <p>email: {employer.email}</p>
+                    {employer.phoneNumber
+                        ? <p>Phone: {employer.phoneNumber}</p>
+                        : null
+                    }
+                </div>
+                <br />
+
+                <div id="singleLinks">
+                    {employer.LinkedInUrl || employer.gitHubUrl
+                        ? <h4>Additional Information about {employer.name}</h4>
+                        : null
+                    }
+                    {employer.LinkedInUrl
+                        ? <a href={employer.LinkedInUrl}>LinkedIn</a>
+                        : null
+                    }
+                    {' '}
+                    {employer.gitHubUrl
+                        ? <a href={employer.gitHubUrl}>GitHub</a>
+                        : null
+                    }
+                </div>
             </div>
         )
     }
