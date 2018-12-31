b1f20b08fc98862e6989b5efa4ab2599ed38f154
Colleen Risso <colleenrisso@gmail.com>
2018-12-30 23:44:13 -0800

    updated for videos

--- server/db/migrations/20181228054026-remove_validations_again.js
+++ server/db/migrations/20181228054026-remove_validations_again.js
@@ -1,5 +1,6 @@
 'use strict';
 // remove all email validations on students and employers if they exist
+// this migration may cause errors if your database didn't originally have a unique email constraint.
 module.exports = {
   up: (queryInterface, Sequelize) => {
     return Promise.all([
